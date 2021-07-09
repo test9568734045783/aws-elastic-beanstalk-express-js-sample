2069aeb64d11cfed8d4452114fabff1dc7e332e8
Cloud User <ec2-user@ip-172-31-6-20.eu-north-1.compute.internal>
2021-07-09 20:28:05 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi again!'));
+app.get('/', (req, res) => res.send('Hi again! and again. Now testing manual reviews'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
